4edbe6cfae929cff9b08d9cc17fb71ee8e234e00
localrepo <ankitmt17@gmail.com>
2026-06-05 14:26:36 +0530

    day 3 collections and generic done

--- backend/sms/src/main/java/com/example/sms/controller/StudentController.java
+++ backend/sms/src/main/java/com/example/sms/controller/StudentController.java
@@ -1,5 +1,7 @@
 package com.example.sms.controller;
 
+import java.util.ArrayList;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,17 +10,17 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.sms.model.Student;
 
 @RestController
-@RequestMapping("/student")
+@RequestMapping("/students")
 @CrossOrigin("*")
 public class StudentController {
 
     @GetMapping
-    public Student getStudent() {
+    public ArrayList<Student> getStudent() {
+      ArrayList<Student> students = new ArrayList<>();
+       students.add(new Student(1, "Ankit", "BE"));
+       students.add(new Student(2, "Harkirat", "ME"));
+       students.add(new Student(3, "Aman", "MCA"));
 
-        return new Student(
-                1,
-                "Ankit",
-                "BE"
-        );
+       return students;
     }
 }
