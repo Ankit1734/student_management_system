9015d4d2ac30c7a8c6f69c0a799b38d40e484560
localrepo <ankitmt17@gmail.com>
2026-06-05 10:14:47 +0530

    Day 2 and 3 done

--- backend/sms/src/main/java/com/example/sms/controller/StudentController.java
+++ backend/sms/src/main/java/com/example/sms/controller/StudentController.java
@@ -17,8 +17,8 @@ public class StudentController {
 
         return new Student(
                 1,
-                "Tushar",
-                "MCA"
+                "Ankit",
+                "BE"
         );
     }
 }
