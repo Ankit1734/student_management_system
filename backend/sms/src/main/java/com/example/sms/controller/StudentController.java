d9d6a24de72ffc93fa968df7a8cccdb372fe94f1
localrepo <ankitmt17@gmail.com>
2026-06-08 22:28:39 +0530

    Day 5 sql server connection done

--- backend/sms/src/main/java/com/example/sms/controller/StudentController.java
+++ backend/sms/src/main/java/com/example/sms/controller/StudentController.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +17,8 @@ import com.example.sms.model.Student;
 @RequestMapping("/students")
 @CrossOrigin("*")
 public class StudentController {
+    @Autowired
+    JdbcTemplate jdbcTemplate;
 
     @GetMapping
     public ArrayList<Student> getStudent() {
@@ -23,16 +27,24 @@ public class StudentController {
        students.add(new Student(2, "Harkirat", "ME"));
        students.add(new Student(3, "Aman", "MCA"));
        students.add(new Student(4,"Tushar","BCA"));
-       students.add(new Student(5,"Aman","BCA"));
+       students.add(new Student(5,"Aman","MCA"));
        students.add(new Student(6,"Priya","BCA"));
 
        return students;
     }
-    @GetMapping("/bca")
-    public List<Student> getBcaStudents(){
+    @GetMapping("/mca")
+    public List<Student> getMCAStudents(){
         return getStudent()
         .stream()
-        .filter(student -> "BCA".equals(student.getCourse()))
+        .filter(student -> "MCA".equals(student.getCourse()))
         .collect(Collectors.toList());
+    }
+    @GetMapping("/count")
+    public int countStudents(){
+        String sql = "SELECT COUNT(*) FROM students";
+        return jdbcTemplate.queryForObject(
+            sql,
+            Integer.class
+    );
     }
 }
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,8 @@ function App(){
     //{"id":1,"name":"John Doe","course":"Computer Science"}
 
     const [students, setStudents] = useState([]);
+    const [count, setCount] = useState(0);
+
     
     const getStudents = async () => {
         
@@ -13,14 +15,19 @@ function App(){
             setStudents(data);
         
     };
-    const getBcaStudents = async () => {
-        
-        const response = await fetch('http://localhost:8080/students/bca');
+
+    const getMCAStudents = async () => {
+        const response = await fetch('http://localhost:8080/students/mca');
         const data = await response.json();
         setStudents(data);
-    
-};
-    
+    };
+
+
+    const fetchTotalStudentCount = async () => {
+        const response = await fetch('http://localhost:8080/students/count');
+        const data = await response.json();
+        setCount(data);
+    }
 
     return (
         <div>
@@ -28,21 +35,27 @@ function App(){
                 fetch Students
             </button>
 
-            <button onClick={getBcaStudents}>
-                Show BCA students
+            <button onClick={getMCAStudents}>
+                fetch MCA Students
             </button>
 
             
+
             <ul>
-            {
-                students.map(student => (
-                    <li key= {student.id}>
+                {students.map(student => (
+                    <li key={student.id}>
                         {student.name} - {student.course}
                     </li>
                 ))}
             </ul>
-            
+
+
+            <button onClick={fetchTotalStudentCount}>
+                Fetch Total Student Count
+            </button>
+            <p>Total Students: {count}</p>
+
         </div>
-    );
+    )
 }
 export default App;
