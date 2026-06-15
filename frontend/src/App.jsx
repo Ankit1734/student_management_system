9c5598c20ac3a150f66c1cc966ae387095c51506
localrepo <ankitmt17@gmail.com>
2026-06-15 14:26:33 +0530

    Day 9 frontend done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,27 @@ function App(){
     
     const [name, setName] = useState("");
     const [course, setCourse] = useState("");
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const login  = async () => {
+        const response = await fetch('http://localhost:8080/auth/login',
+            {
+                method: "POST",
+                headers:{
+                    "Content-Type":
+                    "application/json"
+                },
+                body:JSON.stringify({
+                    username,
+                    password
+                })
+            }
+        );
+    
+        alert("Login Success");
+    }
     
     const getStudents = async () => {
         
@@ -77,20 +98,45 @@ function App(){
             </button>
             <p>Total Students: {count}</p>
 
-           <h1>Student Registration Form</h1>
-           <input 
-           type="text" 
-           placeholder="Name" 
-           value={name} 
-           onChange={(e) => setName(e.target.value)}/>
+        <h1>Student Registration Form</h1>
+        <input 
+        type="text" 
+        placeholder="Name" 
+        value={name} 
+        onChange={(e) => setName(e.target.value)}/>
 
-           <input type="text" 
-           placeholder="Course" 
-           value={course} 
-           onChange={(e) => setCourse(e.target.value0)}/>  
+        <input type="text" 
+        placeholder="Course" 
+        value={course} 
+        onChange={(e) => setCourse(e.target.value0)}/>  
 
-           <button onClick={addStudent}>Register</button>
-           
+        <button onClick={addStudent}>Register</button>
+
+        <h1>Authentication Requires</h1>
+        <input
+        placeholder="Username"
+        value={username}
+        onChange={(e)=>
+        setUsername(
+            e.target.value
+        )}
+    />
+
+    <input
+        placeholder="Password"
+        value={password}
+        onChange={(e)=>
+        setPassword(
+            e.target.value
+        )}
+    />
+
+    <button
+    onClick={login}
+    >
+        Login
+    </button>
+    
         </div>
     )
 }
